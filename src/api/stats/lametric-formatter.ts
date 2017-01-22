d7c237437ce0dbef379243fd63c4e8ebd6d7a7cc
Dolan Miu <dolan_miu@hotmail.com>
2017-01-22 04:50:31 +0000

    added more metrics

--- src/api/stats/lametric-formatter.ts
+++ src/api/stats/lametric-formatter.ts
@@ -13,6 +13,8 @@ export class LaMetricFormatter {
     public format(data: [AggregatedStats, ChampSummary]): ILaMetricOutput {
         const frames: ILaMetricFrame[] = [];
         const aggregatedStats = data[0];
+        const champStats = data[1];
+        const currentYear = new Date().getFullYear();
 
         frames.push({
             text: `Total Assists: ${aggregatedStats.totalAssists}`,
@@ -39,8 +41,46 @@ export class LaMetricFormatter {
             icon: LOGO_ICON_STRING,
         });
 
+        frames.push({
+            text: `Games Played in Ranked ${currentYear}: ${champStats.summary.stats.totalSessionsPlayed}`,
+            icon: LOGO_ICON_STRING,
+        });
+
+        frames.push({
+            text: `W/L Ratio in Ranked ${currentYear}: ${this.getRatio(champStats.summary.stats.totalSessionsWon, champStats.summary.stats.totalSessionsLost)}`,
+            icon: LOGO_ICON_STRING,
+        });
+
+        frames.push({
+            text: `Total Double Kills in Ranked ${currentYear}: ${champStats.summary.stats.totalDoubleKills}`,
+            icon: LOGO_ICON_STRING,
+        });
+
+        frames.push({
+            text: `Total Triple Kills in Ranked ${currentYear}: ${champStats.summary.stats.totalTripleKills}`,
+            icon: LOGO_ICON_STRING,
+        });
+
+        frames.push({
+            text: `Total Quadra Kills in Ranked ${currentYear}: ${champStats.summary.stats.totalQuadraKills}`,
+            icon: LOGO_ICON_STRING,
+        });
+
+        frames.push({
+            text: `Total Penta Kills in Ranked ${currentYear}: ${champStats.summary.stats.totalPentaKills}`,
+            icon: LOGO_ICON_STRING,
+        });
         return {
             frames,
         };
+    }
+
+    private getRatio(firstNumber: number, secondNumber: number): string {
+        const rawRatio = firstNumber / secondNumber;
+
+        if (isNaN(rawRatio)) {
+            return "1.00";
+        }
+        return rawRatio.toFixed(2);
     }
 }
