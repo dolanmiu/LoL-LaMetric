634112c0bc70e1793772818927381de3d7a33395
Dolan Miu <dolan_miu@hotmail.com>
2017-02-04 19:14:40 +0000

    used game typings instead

--- src/api/stats/recent-games-fetcher.ts
+++ src/api/stats/recent-games-fetcher.ts
@@ -6,21 +6,18 @@ export class RecentGamesFetcher {
     constructor(private apiKey: string) {
     }
 
-    public fetchLast(summonerId: number, region: string): Promise<AggregatedStats> {
-        return new Promise<AggregatedStats>((resolve, reject) => {
+    public fetchLast(summonerId: number, region: string): Promise<Game> {
+        return new Promise<Game>((resolve, reject) => {
             request(`https://${region}.api.pvp.net/api/lol/${region}/v1.3/game/by-summoner/${summonerId}/recent?api_key=${this.apiKey}`, {
                 json: true,
-            }, (error, response, body) => {
+            }, (error, response, body: GamesResponse) => {
                 if (response === undefined || (error && response.statusCode !== 200)) {
                     reject(error);
                     logger.error(error);
                     return;
                 }
 
-                const gameResponse = body as StatsResponse;
-                console.log(gameResponse);
-
-                resolve();
+                resolve(body.games[0]);
             });
         });
     }
