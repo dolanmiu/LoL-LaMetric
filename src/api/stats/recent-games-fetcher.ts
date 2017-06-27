7d0e3c298ec107ae729ef2f93eae73f17f2ad242
Dolan Miu <dolan_miu@hotmail.com>
2017-06-28 00:23:18 +0100

    made a new recent games fetcher using v3 api

--- src/api/stats/recent-games-fetcher.ts
+++ src/api/stats/recent-games-fetcher.ts
@@ -6,18 +6,46 @@ export class RecentGamesFetcher {
     constructor(private apiKey: string) {
     }
 
-    public fetchLast(summonerId: number, region: string): Promise<Game> {
+    public fetchLast(summonerId: number, region: Region): Promise<Game> {
         return new Promise<Game>((resolve, reject) => {
-            request(`https://${region}.api.pvp.net/api/lol/${region}/v1.3/game/by-summoner/${summonerId}/recent?api_key=${this.apiKey}`, {
+            const url = `https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/${summonerId}/recent?api_key=${this.apiKey}`;
+            request(url, {
                 json: true,
-            }, (error, response, body: GamesResponse) => {
+            }, (error, response, body: MatchListResponse) => {
                 if (response === undefined || (error && response.statusCode !== 200)) {
                     reject(error);
                     logger.error(error);
                     return;
                 }
 
-                resolve(body.games[0]);
+                if (body.matches.length === 0) {
+                    reject(error);
+                    logger.error(error);
+                    return;
+                }
+
+                return this.fetchMatch(body.matches[0].gameId, summonerId, region);
+            });
+        });
+    }
+
+    private fetchMatch(matchId: number, summonerId: number, region: Region): Promise<MatchParticipant> {
+        return new Promise<MatchParticipant>((resolve, reject) => {
+            const url = `https://euw1.api.riotgames.com/lol/match/v3/matches/${matchId}?api_key=${this.apiKey}`;
+            request(url, {
+                json: true,
+            }, (error, response, body: MatchResponse) => {
+                if (response === undefined || (error && response.statusCode !== 200)) {
+                    reject(error);
+                    logger.error(error);
+                    return;
+                }
+
+                const participant = body.participants.find((p, index) => {
+                    return p.participantId === summonerId;
+                });
+
+                resolve(participant);
             });
         });
     }
