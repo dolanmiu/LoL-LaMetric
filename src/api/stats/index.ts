9c0ceb53bbbd393efdb7478b55da37df914e9c7a
Dolan Miu <dolan_miu@hotmail.com>
2017-06-28 01:56:33 +0100

    Region converter integration

--- src/api/stats/index.ts
+++ src/api/stats/index.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import * as request from "request";
 import * as logger from "winston";
 import { ChampDictionary } from "../../league/champ-dictionary";
+import { RegionConverter } from "../region-converter";
 import { LaMetricFormatter } from "./lametric-formatter";
 import { RecentGamesFetcher } from "./recent-games-fetcher";
 import { SummonerFetcher } from "./summoner-fetcher";
@@ -25,16 +26,19 @@ export class StatsRouter {
     public init(): void {
         this.router.get("/", (req: Request, res: Response) => {
             const name: string = req.query.name;
-            const region: string = req.query.region;
+            const regionString: string = req.query.region;
 
-            if (name === undefined || region === undefined) {
+            if (name === undefined || regionString === undefined) {
                 res.status(400).send("name and region cannot be empty");
                 return;
             }
 
-            this.summonerFetcher.getStats(name, region).then((summoner) => {
-                const lastGamePromise = this.recentGamesFetcher.fetchLast(summoner.id, region);
+            const region = RegionConverter.convert(regionString);
 
+            this.summonerFetcher.getStats(name, region).then((summoner) => {
+                const lastGamePromise = this.recentGamesFetcher.fetchLast(summoner.accountId, region);
+
+                Promise.all([lastGamePromise]).then((stats) => {
                     this.laMetricFormatter.format(stats).then((laMetricOutput) => {
                         res.status(200).json(laMetricOutput);
                     });
