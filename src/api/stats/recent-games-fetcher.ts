11489b5629b0f303450235127972a9fb8d17ca21
Dolan Miu <dolan_miu@hotmail.com>
2017-06-28 01:57:24 +0100

    New recent games fetcher

--- src/api/stats/recent-games-fetcher.ts
+++ src/api/stats/recent-games-fetcher.ts
@@ -6,9 +6,9 @@ export class RecentGamesFetcher {
     constructor(private apiKey: string) {
     }
 
-    public fetchLast(summonerId: number, region: Region): Promise<Game> {
-        return new Promise<Game>((resolve, reject) => {
-            const url = `https://${region}.api.riotgames.com/lol/match/v3/matchlists/by-account/${summonerId}/recent?api_key=${this.apiKey}`;
+    public fetchLast(accountId: number, region: Region): Promise<MatchParticipant> {
+        return new Promise<MatchParticipant>((resolve, reject) => {
+            const url = `https://${region}.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}/recent?api_key=${this.apiKey}`;
             request(url, {
                 json: true,
             }, (error, response, body: MatchListResponse) => {
@@ -24,12 +24,16 @@ export class RecentGamesFetcher {
                     return;
                 }
 
-                return this.fetchMatch(body.matches[0].gameId, summonerId, region);
+                this.fetchMatch(body.matches[0].gameId, accountId, region).then((participant) => {
+                    resolve(participant);
+                }).catch((err) => {
+                    reject(err);
+                });
             });
         });
     }
 
-    private fetchMatch(matchId: number, summonerId: number, region: Region): Promise<MatchParticipant> {
+    private fetchMatch(matchId: number, accountId: number, region: Region): Promise<MatchParticipant> {
         return new Promise<MatchParticipant>((resolve, reject) => {
             const url = `https://${region}.api.riotgames.com/lol/match/v3/matches/${matchId}?api_key=${this.apiKey}`;
             request(url, {
@@ -41,8 +45,12 @@ export class RecentGamesFetcher {
                     return;
                 }
 
+                const participantIdentity = body.participantIdentities.find((p, index) => {
+                    return p.player.accountId === accountId;
+                });
+
                 const participant = body.participants.find((p, index) => {
-                    return p.participantId === summonerId;
+                    return p.participantId === participantIdentity.participantId;
                 });
 
                 resolve(participant);
