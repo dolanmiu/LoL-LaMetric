85360c88789f065da5b31ab4f166367a5be9aba6
Dolan Miu <dolan_miu@hotmail.com>
2017-06-28 02:43:27 +0100

    Updated dependencies

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,6 +8,6 @@ export interface IConfig {
 }
 
 export class Config implements IConfig {
-    public port = process.env.PORT || 9000;
+    public port = parseInt(process.env.PORT, 10) || 9000;
     public apiKey = process.env.RIOT_API_KEY;
 }
